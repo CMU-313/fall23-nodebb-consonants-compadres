277790bf5a8feeebf2e3fecb968ff1a04f6a00d3
Monica Qiu <m1102778586@gmail.com>
2023-10-26 22:36:04 -0400

    added jalangi and mock tests

---
+++ test/mocks/mock_test_jalangi_.js
@@ -0,0 +1,19 @@
+J$.iids = {"9":[1,1,1,8],"17":[1,13,1,16],"25":[1,1,1,17],"27":[1,1,1,12],"33":[1,1,1,18],"41":[1,1,1,18],"49":[1,1,1,18],"57":[1,1,1,18],"nBranches":0,"originalCodeFileName":"../../test/mocks/mock_test.js","instrumentedCodeFileName":"../../test/mocks/mock_test_jalangi_.js","code":"console.log(\"1\");"};
+jalangiLabel0:
+    while (true) {
+        try {
+            J$.Se(41, '../../test/mocks/mock_test_jalangi_.js', '../../test/mocks/mock_test.js');
+            J$.X1(33, J$.M(25, J$.R(9, 'console', console, 2), 'log', 0)(J$.T(17, "1", 21, false)));
+        } catch (J$e) {
+            J$.Ex(49, J$e);
+        } finally {
+            if (J$.Sr(57)) {
+                J$.L();
+                continue jalangiLabel0;
+            } else {
+                J$.L();
+                break jalangiLabel0;
+            }
+        }
+    }
+// JALANGI DO NOT INSTRUMENT
